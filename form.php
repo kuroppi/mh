f0e220fbc63b76fff96edf75996979dac463ef54
B P <str.kuroppi@gmail.com>
2015-08-25 13:11:52 -0700

    MH
    Added header text

---
+++ form.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Chelsea+Market|Yanone+Kaffeesatz:400,700,300|Noticia+Text'>
+
+    <link rel="stylesheet" href='css/font-awesome.min.css'>
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- JAVASCRIPT
+    -------------------------------------------------- -->
+    <script src="js/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <main id="contact" class="container">
+
+        <section class="row">
+            <section class="two columns">
+                <div id="brand"><a href="index.html">Magic<br> Hours<br><span>Preschool</span></a></div>
+
+                <nav id="vertNav">
+                    <ul>
+                        <li><a href="about.html"><i class="fa fa-user fa-2x"></i> <br/>about</a></li>
+                        <li><a href="info.html"><i class="fa fa-info-circle fa-2x"></i> <br/>info&nbsp;</a></li>
+                        <li><a href="register.html"><i class="fa fa-pencil fa-2x"></i><br/>register</a></li>
+                        <li><a href="calendar.html"><i class="fa fa-calendar fa-2x"></i><br/>calendar</a></li>
+                        <li><a href="contact.html"><i class="fa fa-phone fa-2x"></i> <br/>contact</a></li>
+                    </ul>
+                </nav>
+
+                <footer id="vertNavFoot">
+                    <div id="footerLinks">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                        <a href=""><i class="fa fa-facebook"></i></a>
+                    </div>
+                    <div>
+                        <p>8330 Westmore Rd
+                            <br>San Diego, CA, 92126
+                            <br>858-271-8383
+                            <br>State License #376700608</p>
+                    </div>
+
+                </footer>
+            </section>
+
+            <section class="ten columns">
+                <section id="contactInfo" class="row">
+                    <article id="contactDetailsLeft" class="one-half column">
+
+                <p><?php ?></p>
+
+                    </article>
+                    <article id="contactDetailsRight" class="one-half column">
+                        <h2>Contact Info</h2>
+                        <p>
+                            123 Westmore
+                            <br> Mira Mesa, CA 92123
+                            <br> 858-5555-1212
+                        </p>
+                        <h2>Schedule A Tour</h2>
+                        <p>Our campus is centrally located in the Mira Mesa neighborhood of San Diego, CA. There are 3 full-sized classrooms, including a dedicated arts & crafts area and cafeteria. The outdoor playground is nearly 50% of the entire campus. </p>
+                        <br>
+                        <a href="#">Schedule A Tour</a>
+
+                        <br>
+                        <h3>Looking for a Staff Member?</h3>
+                        <a href="staff.html">Learn More About Our Staff</a>
+
+
+                    </article>
+                </section>
+
+            </section>
+
+
+
+        </section>
+
+
+
+    </main>
+
+
+
+
+</body>
+
+</html>
